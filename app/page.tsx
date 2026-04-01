f944518577ea2773aa1528064a096c28dc39583c
Jerson Galabo <jgalabo.myown@gmail.com>
2026-04-01 13:27:02 +0800

    updated the landing page

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Navbar from "@/components/navbar";
 export default function Home() {
   return (
     <main className="flex items-center justify-center h-screen bg-black text-white">
-      <h1 className="text-3xl font-bold">MOBA Platform 🚀</h1>
+      <h1 className="text-3xl font-bold">Please Login to View Full App Features</h1>
     </main>
   )
 }
